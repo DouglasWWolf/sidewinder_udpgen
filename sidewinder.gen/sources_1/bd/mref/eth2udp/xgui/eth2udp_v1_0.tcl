0ea8277e65f27cbf792c7c0a0212f7b93e4dda84
DouglasWWolf <douglaswolf@yahoo.com>
2023-07-27 17:23:58 -0700

    No commit message

--- sidewinder.gen/sources_1/bd/mref/eth2udp/xgui/eth2udp_v1_0.tcl
+++ sidewinder.gen/sources_1/bd/mref/eth2udp/xgui/eth2udp_v1_0.tcl
@@ -14,6 +14,7 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "SRC_IP3" -parent ${Page_0}
   ipgui::add_param $IPINST -name "SRC_MAC" -parent ${Page_0}
   ipgui::add_param $IPINST -name "SRC_PORT" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "SWIDTH" -parent ${Page_0}
 
 
 }
@@ -117,6 +118,15 @@ proc validate_PARAM_VALUE.SRC_PORT { PARAM_VALUE.SRC_PORT } {
 	return true
 }
 
+proc update_PARAM_VALUE.SWIDTH { PARAM_VALUE.SWIDTH } {
+	# Procedure called to update SWIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.SWIDTH { PARAM_VALUE.SWIDTH } {
+	# Procedure called to validate SWIDTH
+	return true
+}
+
 
 proc update_MODELPARAM_VALUE.SRC_MAC { MODELPARAM_VALUE.SRC_MAC PARAM_VALUE.SRC_MAC } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -171,5 +181,10 @@ proc update_MODELPARAM_VALUE.SRC_PORT { MODELPARAM_VALUE.SRC_PORT PARAM_VALUE.SR
 proc update_MODELPARAM_VALUE.DST_PORT { MODELPARAM_VALUE.DST_PORT PARAM_VALUE.DST_PORT } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.DST_PORT}] ${MODELPARAM_VALUE.DST_PORT}
+}
+
+proc update_MODELPARAM_VALUE.SWIDTH { MODELPARAM_VALUE.SWIDTH PARAM_VALUE.SWIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.SWIDTH}] ${MODELPARAM_VALUE.SWIDTH}
 }
 
